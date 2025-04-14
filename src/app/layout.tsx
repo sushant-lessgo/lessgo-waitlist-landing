5c8ab7d254bd3d6c93de2864510eda3a81548ebf
Sushant Jain <sushant@lessgo.ai>
2025-04-15 00:21:58 +0200

    google analtics deployed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from 'next/script'
 import { GeistSans, GeistMono } from 'geist/font';
 import "./globals.css";
 
@@ -14,6 +15,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+{/* Google Analytics */}
+<Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-DM2YZB9VYG"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DM2YZB9VYG');
+            `,
+          }}
+        />
+
+      </head>
       <body className={`${GeistSans.className} antialiased`}>
         {children}
       </body>
